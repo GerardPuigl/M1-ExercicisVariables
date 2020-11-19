aa02921b059330ff136e1672e46735ece23904c2
gerardpuiglatorre <72300632+gerardpuiglatorre@users.noreply.github.com>
2020-11-19 18:48:29 +0100

    Update VariablesAPP.java
    correcciones de estilo

--- src/VariablesAPP.java
+++ src/VariablesAPP.java
@@ -3,35 +3,36 @@ public class VariablesAPP {
 
 	public static void main(String[] args) {
 		
-		//Declaración y inicialización de nombre,apellidos y fecha de nacimiento
+		//DeclaraciÃ³n y inicializaciÃ³n de nombre,apellidos y fecha de nacimiento
 		
 		String nom="Pep",cognom1="Botella",cognom2="Mares";
 		int dia=18,mes=01,any=1984;
 		
-		//Cálculo de números años de traspaso
+		//CÃ¡lculo de nÃºmeros aÃ±os de traspaso respecto al 1948 como aÃ±o de referencia.
 		
 		final int REFTRASPAS=1948;
 		int ntraspas;
 		ntraspas=(any-REFTRASPAS)/4;
 		
-		//Búcle de años de traspaso y cálculo si es un año de traspaso
+		//BÃºcle de aÃ±os de traspaso y cÃ¡lculo si es un aÃ±o de traspaso
 		
 		int anytraspas=REFTRASPAS;
 		for (int i = 0; i < ntraspas; i++) {
 			anytraspas=anytraspas+4;				
-			}
+		}
 		boolean bool=(any==anytraspas);
 
-		//Unir la informació en variables e imprimir
+		//Unir la informaciÃ³ en variables e imprimir
 		
 		String nomcomplet= nom+" "+cognom1+" "+cognom2;
 		String dataneixement = dia+"/"+mes+"/"+any;
 		System.out.println(nomcomplet);
 		System.out.println(dataneixement);		
 		if (bool){
-			System.out.println("El meu any de naixement és de traspàs.");
-			}else{
-			System.out.println("El meu any de naixement no és de traspàs.");	
-			}
+			System.out.println("El meu any de naixement Ã©s de traspÃ s.");
+		}else{
+			System.out.println("El meu any de naixement no Ã©s de traspÃ s.");
+			
 		}
+	}
 }
