0d1f5e4a0458aea76f4348121dbc74868313ece1
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-19 16:55:12 +0100

    Exercici 1 i 2

---
+++ src/Fase1.java
@@ -0,0 +1,12 @@
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+		
+		String nom="Pep",cognom1="Botella",cognom2="Mares";
+		int dia=18,mes=01,any=1971;
+		System.out.println(nom+" "+cognom1+" "+cognom2);
+		System.out.println(dia+"/"+mes+"/"+any);
+		}
+
+}
---
+++ src/Fase2.java
@@ -0,0 +1,15 @@
+public class Fase2{
+	public static void main(String[]args) {
+		
+		int REF=1948;
+		int ANYNEIX=1984;
+		int ntraspas;
+		ntraspas=(ANYNEIX-REF)/4;
+		System.out.println(ntraspas);
+			
+	}
+	
+	
+	
+	
+}
