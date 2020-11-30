5ae4326325ff9f79eaeba90686d3a0268355bd41
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-30 19:57:08 +0100

    Fase 2 Completada

--- src/VariablesAPP.java
+++ src/VariablesAPP.java
@@ -2,13 +2,19 @@
 public class VariablesAPP {
 
 	public static void main(String[] args) {
+
+		// Declaración y inicialización de nombre,apellidos y fecha de nacimiento
+		String nom = "Gerard", cognom1 = "Puig", cognom2 = "Latorre";
+		int dia = 01, mes = 01, any = 1990;
+
+		System.out.println(nom + " " + cognom1 + " " + cognom2);
+		System.out.println(dia + "/" + mes + "/" + any);
 		
-		//Declaración y inicialización de nombre,apellidos y fecha de nacimiento
+		// Cáculo de numeros años de traspaso respecto al 1948 como año de referencia.
+		final int REFTRASPAS = 1948;
+		int ntraspas;
+		ntraspas = (any - REFTRASPAS) / 4;
 		
-		String nom="Gerard",cognom1="Puig",cognom2="Latorre";
-		int dia=01,mes=01,any=1990;
-		
-		System.out.println(nom+" "+cognom1+" "+cognom2);
-		System.out.println(dia+"/"+mes+"/"+any);
+		System.out.println("Anys de traspas entre el teu aniversari i el 1948: "+ntraspas);
 	}
 }
